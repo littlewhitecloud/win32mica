4cea1369171b4e9ced1c03cec2d03dbf001f5d83
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2023-07-26 20:05:01 +0800

    Delete test.cpp

--- example/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-#include "plugin.h" 
-#pragma comment(lib, "plugin.lib")
-#pragma comment(lib, "user32.lib")
-
-int main() {
-	ApplyDocumentMica(FindWindowW(NULL, L"Mica Example"), true);
-	return 0;
-} 
